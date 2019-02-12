616ccc1356be0a8339322e27d47d2f005b1eb4cd
Taylor Sturtz <taylor.sturtz@ytel.com>
2019-02-11 21:47:06 -0800

    initial stuff

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  makeACall = async () => {
+    try {
+      const response = await fetch('http://chatbot.test/match', {
+        method: 'post',
+        body: JSON.stringify({
+          "start_date": "2019-01-18 18:45:00",
+	        "end_date": "2019-01-19 18:45:00"
+        }),
+      })
+      const json = await response.json();
+      console.log('-->', json);
+    } catch (err) {
+
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={this.makeACall}>click meh</button>
       </div>
     );
   }
